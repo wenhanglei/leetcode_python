350da7d98c92bdaac0a9223e65e8848e529d1ec9
wenhanglei <wenhanglei@gmail.com>
2021-07-26 23:40:08 +0800

    feat:complement perfect squares

--- array/perfect_squares.py
+++ array/perfect_squares.py
@@ -6,30 +6,39 @@ itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
 """
 
 
+
+
 class Solution(object):
     def numSquares(self, n):
         """
         :type n: int
         :rtype: int
         """
-        pass
+        l = {i ** 2: 1 for i in range(1, n+1) if i ** 2 <= n}
+        m = [i ** 2 for i in range(1, n+1) if i ** 2 <= n]
+        if m[-1] == n:
+            return 1
+        for i in range(2, n+1):
+            self.search(l, m, i, i, 0, n)
+            if n in l:
+                return l[n]
 
-
-    def isSquare(self, n):
-        for i in range(n):
-            if i * i == n:
-                return True
-            elif i * i < n:
-                continue
-            else:
-                return False
+    def search(self, l, m, num, count, ret, limit):
+        if ret > limit:
+            return
+        elif count == 0:
+            if ret not in l:
+                l[ret] = num
+            return
+        else:
+            for i in m:
+                self.search(l, m, num, count - 1, i + ret, limit)
 
 
 if __name__ == "__main__":
     s = Solution()
     cases = []
-    cases.append(12)
-    cases.append(13)
+    cases.append(7168)
     for case in cases:
         ret = s.numSquares(case)
         print("Input: n = %s" % case)
