a846071d5769c7d9a94679cadd1093c1fe88fb1c
wenhanglei <wenhanglei@gmail.com>
2021-08-27 21:04:01 +0800

    feat:super ugly number iterate solution v1

--- array/super_ugly_number.py
+++ array/super_ugly_number.py
@@ -14,4 +14,39 @@ class Solution(object):
         :type primes: List[int]
         :rtype: int
         """
+        if n == 1:
+            return 1
+        l = [1]
+        for k in range(n-1):
+            current = l[-1]
+            idx = 0
+            m = current
+            for j in primes[::-1]:
+                while idx < len(l):
+                    if j * l[idx] > current:
+                        break
+                    idx += 1
+                else:
+                    break
+                if m == current:
+                    m = j * l[idx]
+                elif j * l[idx] < m:
+                    m = j * l[idx]
+            if m > current:
+                l.append(m)
+        return l[-1]
 
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    n = 12
+    primes = [2, 7, 13, 19]
+    s = Solution()
+    r = s.nthSuperUglyNumber(n, primes)
+    print(r)
