d7eb14c9314307415d8c563297b2f991973f0c44
wenhanglei <wenhanglei@gmail.com>
2021-09-02 22:45:49 +0800

    feat:straightforward resolution Time Limit Exceeded

--- string/remove_duplicate_letters.py
+++ string/remove_duplicate_letters.py
@@ -10,13 +10,37 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        r = set(s)
-        l = list(r)
-        return ''.join(l)
+        m = {}
+        for idx, i in enumerate(s):
+            if i not in m:
+                m[i] = []
+            m[i].append(idx)
+        keys = [i for i in m]
+        self.text = None
+        self.traverse(keys, 0, m, [], list(s))
+        return self.text
+
+    def traverse(self, keys, next, m, r, l):
+        if next >= len(keys):
+            t = self.merge(r[:], l)
+            if self.text is None:
+                self.text = t
+            else:
+                self.text = min(self.text, t)
+            return
+        for i in m[keys[next]]:
+            r.append(i)
+            self.traverse(keys, next+1, m, r, l)
+            del r[-1]
+
+    def merge(self, l, o):
+        l.sort()
+        return ''.join([o[i] for i in l])
 
 
 if __name__ == "__main__":
-    s = "bcabc"
+    s = "cbacdcbc"
     sol = Solution()
     r = sol.removeDuplicateLetters(s)
     print(r)
+
