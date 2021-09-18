9f2d1a19b5ca19081ccc73e7bd0fd80f46db6f31
wenhanglei <wenhanglei@gmail.com>
2021-09-18 21:58:19 +0800

    feat:wiggle sort ugly version

--- array/wiggle_sort_II.py
+++ array/wiggle_sort_II.py
@@ -11,17 +11,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)-1):
-            if i%2 == 0:
-                if nums[i+1] < nums[i]:
-                    nums[i], nums[i+1] = nums[i+1], nums[i]
-            else:
-                if nums[i+1] > nums[i]:
-                    nums[i], nums[i+1] = nums[i+1], nums[i]
+        nums.sort(reverse=True)
+        size = len(nums)
+        limit = size // 2
+        itl = iter(nums[:limit])
+        itr = iter(nums[limit:])
+        for i in range(size):
+            nums[i] = next(itr) if i % 2 == 0 else next(itl)
+
 
 
 if __name__ == "__main__":
-    nums = [5,5,5,4,4,4,4]
+    nums = [4,5,5,6]
     s = Solution()
     s.wiggleSort(nums)
     print(nums)
