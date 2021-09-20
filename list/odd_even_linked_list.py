23dc83c997465f3b8931c61d1d19b1bac367f877
wenhanglei <wenhanglei@gmail.com>
2021-09-20 21:32:39 +0800

    feat:odd even linked list simple solution

--- list/odd_even_linked_list.py
+++ list/odd_even_linked_list.py
@@ -9,15 +9,59 @@ Note that the relative order inside both the even and odd groups should remain a
 You must solve the problem in O(1) extra space complexity and O(n) time complexity.
 """
 
+
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution(object):
     def oddEvenList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
+        sintinel = head
+        oddTail = head
+        if sintinel is None:
+            return sintinel
+        evenHead = head.next
+        evenTail = head.next
+        if evenHead is None:
+            return sintinel
+        next = head.next.next
+        while True:
+            if next is None:
+                oddTail.next = evenHead
+                return sintinel
+            elif evenTail.next == next:
+                oddTail.next = next
+                oddTail = oddTail.next
+                evenTail.next = None
+            elif oddTail.next == next:
+                evenTail.next = next
+                evenTail = evenTail.next
+                oddTail.next = None
+            next = next.next
+
+
+
+if __name__ == "__main__":
+    node5 = ListNode(5, None)
+    node4 = ListNode(4, node5)
+    node3 = ListNode(3, node4)
+    node2 = ListNode(2, node3)
+    node1 = ListNode(1, node2)
+
+    s = Solution()
+    s.oddEvenList(node1)
+
+    node = node1
+    while True:
+        print(node.val, end=" ")
+        if node.next is None:
+            break
+        node = node.next
 
